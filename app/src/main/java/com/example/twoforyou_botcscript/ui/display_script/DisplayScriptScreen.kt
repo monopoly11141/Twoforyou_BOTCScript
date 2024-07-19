298bdbcbffb54a9c544f97bdba0e684d62a40057
monopoly1114 <78067529+monopoly11141@users.noreply.github.com>
2024-07-19 17:07:01 +0900

    working on search function

--- app/src/main/java/com/example/twoforyou_botcscript/ui/display_script/DisplayScriptScreen.kt
+++ app/src/main/java/com/example/twoforyou_botcscript/ui/display_script/DisplayScriptScreen.kt
@@ -10,10 +10,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -47,11 +50,25 @@ fun DisplayScriptScreen(
 
     var addScriptButtonClicked by remember { mutableStateOf(false) }
 
+    var searchText by remember {mutableStateOf("")}
+
     Scaffold { paddingValues ->
         Column(
             modifier = Modifier
                 .padding(paddingValues)
         ) {
+            TextField(
+                value = searchText,
+                onValueChange = {
+                    searchText = it
+                },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                trailingIcon = {
+                    Icons.Filled.Search
+                }
+            )
+
             LazyColumn(
                 modifier = Modifier
                     .weight(1f)
